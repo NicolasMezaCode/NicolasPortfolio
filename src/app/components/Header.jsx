82befdc582683070d4e8c40a3b77c9400f82a177
Nicolas <nickismeza@gmail.com>
2023-02-19 16:35:17 -0800

    change in header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,18 +6,18 @@ export default function Header () {
     <header className={`${styles.header}`}>
       <nav className={`${styles.nav} inner`}>
         <div className={styles.logoContainer}>
-          <a href=""> <Image src='/images/logo.png' alt='logo' width={201} height={31} /></a>
+          <a className={styles.logo} href=""><Image src='/images/logo.png' alt='logo' width={178} height={26} /></a>
           <ul className={styles.listcontainer}>
-            <li className={styles.marginright}><a href="">Home</a></li>
-            <li className={styles.marginright}><a href="">About</a></li>
-            <li className={styles.marginright}><a href="">Projects</a></li>
+            <li className={`${styles.marginright} ${styles.font700}`}><a href="">Home</a></li>
+            <li className={`${styles.marginright} ${styles.font700}`}><a href="">About</a></li>
+            <li className={`${styles.marginright} ${styles.font700}`}><a href="">Projects</a></li>
           </ul>
         </div>
-        <div className={styles.contactcsontainer}>
+        <div className={styles.contactcontainer}>
           <ul className={styles.listcontainer}>
-            <li className={styles.marginright}><a href="">Contact</a></li>
-            <li className={styles.marginright}><a href="">Blog</a></li>
-            <li className={styles.marginright}><a href="">Github</a></li>
+            <li className={`${styles.marginright} ${styles.font300}`}><a href="">Contact</a></li>
+            <li className={`${styles.marginright} ${styles.font300}`}><a href="">Blog</a></li>
+            <li className={`${styles.marginright} ${styles.font300}`}><a href="">Github</a></li>
           </ul>
         </div>
       </nav>
