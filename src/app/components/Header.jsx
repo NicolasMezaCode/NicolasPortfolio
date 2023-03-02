0aa437bf82f29f8202ee4ff53ecfbb9c2cb7e049
Nicolas <nickismeza@gmail.com>
2023-03-01 23:21:35 -0800

    changes in header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,11 +6,11 @@ export default function Header () {
     <header className={`${styles.header}`}>
       <nav className={`${styles.nav} inner`}>
         <div className={styles.logoContainer}>
-          <a className={styles.logo} href=""><Image src='/images/logo.png' alt='logo' width={178} height={26} /></a>
+          <a className={styles.logo} href=""><p>Nicolas Meza</p></a>
           <ul className={styles.listcontainer}>
-            <li className={`${styles.marginright} ${styles.font700}`}><a href="">Home</a></li>
-            <li className={`${styles.marginright} ${styles.font700}`}><a href="">About</a></li>
-            <li className={`${styles.marginright} ${styles.font700}`}><a href="">Projects</a></li>
+            <li className={`${styles.font300}`}><a href="" className={styles.btnRef}>HOME</a></li>
+            <li className={`${styles.font300}`}><a href="" className={styles.btnRef}>ABOUT</a></li>
+            <li className={`${styles.font300}`}><a href="" className={styles.btnRef}>PROJECTS</a></li>
           </ul>
         </div>
         <div className={styles.contactcontainer}>
