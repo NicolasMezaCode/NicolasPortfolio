1ca87f8b439e4066d5ae8320573f2324c414ddba
Nicolas <nickismeza@gmail.com>
2023-05-05 14:30:51 -0700

    fixing images

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Title() {
   return (
-    <section className='bg-hero-pattern flex justify-center items-center  pt-10 pb-10'>
+    <section className="bg-[url('./assets/composition-book-cover-black-and-white-background-abstract-wallpaper-texture-or-pattern-grain-or-noise-marble-pattern-fabric-and-surface-wrapping-paper-flat-de.jpg')] flex justify-center items-center  pt-10 pb-10">
         <div className='bg-primary rounded-sm  w-fit sm:p-10 px-2 py-4 flex justify-center items-center  flex-col '>
             <h1 className='text-3xl font-bold text-white opacity-90 tracking-wide mb-6 sm:text-4xl md:text-5xl '>Nicolas Meza</h1>
             <h2 className='font-light text-white opacity-90 text-lg text-center max-w-xs sm:text-2xl sm:max-w-sm  '>FullStack developer, passionate about technology, occasional writer.</h2>
