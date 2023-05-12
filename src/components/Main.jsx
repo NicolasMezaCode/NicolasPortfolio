1468fb8583214b8850d6982284d2b13ef9685e61
Nicolas <nickismeza@gmail.com>
2023-05-12 13:13:18 -0700

    fixing hover on mobile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,20 +21,20 @@ export default function Main() {
                 <h3 className='font-hand font-bold text-3xl sm:text-5xl px-4 py-2  text-black sm:px-6 sm:py-3'>GAMESS</h3>
           </div>
           <a href="https://gamess.vercel.app/">
-          <figure className='w-fit mx-5 mb-7 rounded-lg container sketchy relative'>
+          <figure className='w-fit mx-5 mb-7 rounded-lg md:container sketchy relative'>
             <div className="relative">
               <img className='h-auto w-80 sm:w-[600px] lg:w-[800px]' src={gamess} alt="screenshot of the project" />
               <div className="absolute inset-0 flex items-center flex-col justify-center bg-bluePostit opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                <div className="text-black text-center m-5">
-                  <h3 className="text-2xl font-bold mb-2">Level up your gaming experience with Gamess! Discover and explore your favorite games using the IGDB database, while Firebase ensures secure authentication and data storage. </h3>                  
+                <div className="text-black text-center m-3 sm:m-5">
+                  <h3 className="text-xs sm:text-2xl font-semibold sm:mb-2">Level up your gaming experience with Gamess! Discover and explore your favorite games using the IGDB database, while Firebase ensures secure authentication and data storage. </h3>                  
                 </div>
                 <span className='flex gap-3'>
-                  <ReactLogo className="h-12 w-12 text-black" />
-                  <HtmlLogo className="h-12 w-12 text-black" />
-                  <CssLogo className="h-12 w-12 text-black" />
-                  <JsLogo className="h-12 w-12 text-black" />
-                  <Node className="h-12 w-12 text-black" />
-                  <Data className="h-12 w-12 text-black" />
+                  <ReactLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <HtmlLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <CssLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <JsLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <Node className="h-auto w-6 sm:w-12 text-black" />
+                  <Data className="h-auto w-6 sm:w-12 text-black" />
                 </span>
               </div>
             </div>
@@ -44,18 +44,18 @@ export default function Main() {
                 <h3 className='font-hand font-bold text-3xl sm:text-5xl px-4 py-2  text-black sm:px-6 sm:py-3'>POMOTIMER</h3>
           </div>
           <a href="https://pomotimertechnique.vercel.app/">
-          <figure className='w-fit mx-5 mb-7 rounded-lg container sketchy relative'>
+          <figure className='w-fit mx-5 mb-7 rounded-lg md:container sketchy relative'>
             <div className="relative">
               <img className='h-auto w-80 sm:w-[600px] lg:w-[800px]' src={pomodoro} alt="screenshot of the project" />
               <div className="absolute inset-0 flex items-center flex-col justify-center bg-yellowPostit opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                <div className="text-black text-center m-5">
-                  <h3 className="text-2xl font-bold mb-2">Boost your productivity with PomoTimer! This React project empowers you to manage tasks effortlessly and stay focused using a built-in timer.</h3>                  
+                <div className="text-black text-center m-3 sm:m-5">
+                  <h3 className="text-xs sm:text-2xl font-semibold  sm:mb-2">Boost your productivity with PomoTimer! This React project empowers you to manage tasks effortlessly and stay focused using a built-in timer.</h3>                  
                 </div>
                 <span className='flex gap-3'>
-                  <ReactLogo className="h-12 w-12 text-black" />
-                  <HtmlLogo className="h-12 w-12 text-black" />
-                  <CssLogo className="h-12 w-12 text-black" />
-                  <JsLogo className="h-12 w-12 text-black" />
+                  <ReactLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <HtmlLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <CssLogo className="h-auto w-6 sm:w-12 text-black" />
+                  <JsLogo className="h-auto w-6 sm:w-12 text-black" />
                 </span>
               </div>
             </div>
