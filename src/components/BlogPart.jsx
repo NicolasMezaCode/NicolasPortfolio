8cb3d9b6a5ad77adfafde406b426b9c0ac69dc51
Nicolas <nickismeza@gmail.com>
2023-05-26 14:31:54 -0700

    changes on margin

--- src/components/BlogPart.jsx
+++ src/components/BlogPart.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function BlogPart() {
   return (
     <div className='flex flex-col justify-center items-center mb-9 md:mb-16 px-8 sm:px-12 md:px-16'>
-       <div className='bg-black opacity-90 rounded-lg w-fit  mb-6 md:mb-12'>
+       <div className='bg-black opacity-90 rounded-lg w-fit  mb-9 md:mb-12'>
             <h3 className='font-bold text-2xl text-white p-3 tracking-wide'>Some of my recent Posts</h3>
         </div>
         {articles.map((article) => (
